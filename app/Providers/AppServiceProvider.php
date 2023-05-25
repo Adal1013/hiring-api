5e93f94e311a88abd98dd97a5e5ada7e3428784b
Adal <adalberto.avila@activeone.co>
2023-05-25 07:05:14 -0500

    add services and repositories to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Repositories\Auth\AuthRepository;
+use App\Http\Repositories\Auth\AuthRepositoryImpl;
+use App\Http\Services\Auth\AuthService;
+use App\Http\Services\Auth\AuthServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +17,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Repositories
+        $this->app->bind(AuthRepository::class, AuthRepositoryImpl::class);
+
+        // Services
+        $this->app->bind(AuthService::class, AuthServiceImpl::class);
     }
 
     /**
