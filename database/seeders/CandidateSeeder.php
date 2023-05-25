da6ac4232d41f264e983ac4a7ab5e160fe2d4aa3
Adal <adalberto.avila@activeone.co>
2023-05-25 07:32:18 -0500

    add candidates seeder

---
+++ database/seeders/CandidateSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Candidate;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class CandidateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $creator = User::where('username', 'adal123')->first();
+        $owner = User::where('username', 'jose123')->first();
+        $candidates = [
+            [
+                'name' => 'David Fernandez',
+                'source' => 'Linkedin',
+                'owner' => $owner->id,
+                'created_by' => $creator->id,
+            ],
+            [
+                'name' => 'Maria Gonzales',
+                'source' => 'Indeed',
+                'owner' => $owner->id,
+                'created_by' => $creator->id,
+            ],
+        ];
+        foreach ($candidates as $candidate) {
+            Candidate::create($candidate);
+        }
+    }
+}
