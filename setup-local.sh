0ac12f24a9fecc4b0a5e42618b6b0e39187495cd
Adal <adalberto.avila@activeone.co>
2023-05-26 16:20:42 -0500

    fix docker-compose.yml and setup-local.sh

--- setup-local.sh
+++ setup-local.sh
@@ -2,49 +2,35 @@
 
 docker-compose kill
 
-echo ''
 echo '----- Setup Laravel'
-echo ''
 
 docker-compose build
-
 docker-compose up -d
 
-echo ''
 echo '----- copy .env '
-echo ''
 
 cp .env.example .env
 
-echo ''
 echo '----- composer install | permissions'
-echo ''
 
 docker exec hiring-api composer install
 docker exec hiring-api chmod -R 775 storage
 docker exec hiring-api chown -R 1000:www-data storage bootstrap/cache
 
-echo ''
 echo '----- Generate Key'
-echo ''
 
 docker exec hiring-api php artisan key:generate
 
-echo ''
 echo '----- Generate secret'
-echo ''
 
-docker exec hiring-api php artisan jwt:secret
+yes | docker exec -i hiring-api php artisan jwt:secret
 
-echo ''
 echo '----- Run Migrations'
-echo ''
 
 docker exec hiring-api php artisan migrate --seed
 
-echo ''
 echo '----- Starting Application'
-echo ''
 
 docker-compose kill
-docker-compose up
+docker exec -d hiring-api php artisan serve --host=0.0.0.0 --port=8001
+docker-compose up -d
