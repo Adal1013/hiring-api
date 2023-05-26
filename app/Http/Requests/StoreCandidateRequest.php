1dcfe9f6ee5eddbfcf1f8dc5ae62a6f243f93569
Adal <adalberto.avila@activeone.co>
2023-05-25 21:38:19 -0500

    add request for store candidate

---
+++ app/Http/Requests/StoreCandidateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCandidateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'source' => 'required|string|max:50',
+            'owner' => 'required|exists:users,id'
+        ];
+    }
+}
