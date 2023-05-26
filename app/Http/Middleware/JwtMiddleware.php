c508c7af9a82cb4e58cb3399c2daa2f8211023ca
Adal <adalberto.avila@activeone.co>
2023-05-26 14:40:18 -0500

    validate if user is active

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use App\Exceptions\InactiveUserException;
 use Closure;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -23,13 +26,18 @@ class JwtMiddleware
     public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         try {
-            JWTAuth::parseToken()->authenticate();
-        } catch (\Exception $e) {
+            JWTAuth::parseToken();
+            if (!Auth::user()->is_active) {
+                throw new InactiveUserException();
+            }
+        } catch (Exception $e) {
             $errorMessage = 'Token not found';
             if ($e instanceof TokenInvalidException) {
                 $errorMessage = 'Token invalid.';
             } elseif ($e instanceof TokenExpiredException) {
                 $errorMessage = 'Token expired.';
+            } elseif ($e instanceof InactiveUserException) {
+                $errorMessage = 'User is not active.';
             }
             return response()->json(
                 [
